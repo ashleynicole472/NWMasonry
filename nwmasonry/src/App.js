c04bedf89a6eef2cdd6eede3c9c8805d2d26cecc
ashleynicole472 <asha.4720@gmail.com>
2018-08-09 16:34:35 -0700

    Adding component folders

--- nwmasonry/src/App.js
+++ nwmasonry/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import About from './components/About/about.js';
+import Contact from './components/Contact/contact.js';
+import Reviews from './components/CustomerReviews/reviews.js';
+import Footer from './components/Footer/footer.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>
+        <About />
+        <Contact />
+        <Reviews />
+        <Footer />
       </div>
     );
   }
