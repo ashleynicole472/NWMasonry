0b1c64c2d6fefa0572b5a13b18340a8d76a7f598
ashleynicole472 <asha.4720@gmail.com>
2018-08-15 15:08:41 -0700

    Setting up component folders, adding bootstrap

--- nwmasonry/src/App.js
+++ nwmasonry/src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import About from './Components/About/about.js';
-import Contact from './Components/Contact/contact.js';
-import Reviews from './Components/CustomerReviews/reviews.js';
-import Footer from './Components/Footer/footer.js'
+import Navbar from './Components/NavBar/NavBar.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <About />
-        <Contact />
-        <Reviews />
-        <Footer />
+        <NavBar />
       </div>
     );
   }
