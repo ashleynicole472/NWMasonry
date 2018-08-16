383482ee0a7d0d22be716a33efae91276c349d0d
ashleynicole472 <asha.4720@gmail.com>
2018-08-15 22:52:52 -0700

    Working on footer

--- nwmasonry/src/components/Footer/footer.js
+++ nwmasonry/src/components/Footer/footer.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import './Footer.css';
 
 class Footer extends Component {
     render(){
         return(
             <div className="footer-container">
                 <div className="row">
-                    <div className="col-md-12">
-                        <h1>Inside footer</h1>
+                    <div className="col-md-4">
+                        <h4>Social</h4><hr/>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div className="col-md-4">
+                        <h4>BBB</h4><hr/>
+                        <ul>
+                            <li>
+                                <a href="https://www.bbb.org/us/wa/vancouver/profile/mason-contractors/nw-masonry-llc-1296-22649156" target="#">
+                                    Better Business Burearu A+
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <div className="col-md-4">
+                        <h4>Contact</h4><hr />
                     </div>
                 </div>
             </div>
