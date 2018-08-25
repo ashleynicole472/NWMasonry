f0f0e02cdc0fc48e36dfd730efd982dad7a61585
ashleynicole472 <asha.4720@gmail.com>
2018-08-25 13:19:33 -0700

    Added react router, routing pages correctly, begining to start work on about us details

--- nwmasonry/src/App.js
+++ nwmasonry/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
 import NavBar from './Components/NavBar/NavBar.js';
 import WelcomeSign from './Components/WelcomeSign/WelcomeSign.js';
 import About from './Components/AboutUs/AboutUs.js';
@@ -6,20 +8,24 @@ import Reviews from './Components/CustomerReviews/Reviews.js';
 import Services from './Components/ConstructionServices/ConstructionServices.js';
 import Gallery from './Components/ProjectGallery/ProjectGallery.js';
 import Footer from './Components/Footer/Footer.js';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <WelcomeSign />
-        <About />
-        <Reviews />
-        <Gallery />
-        <Services />
-        <Footer />
-      </div>
+        <BrowserRouter>
+            <div>
+                <NavBar />
+                    <Route path='/' component={WelcomeSign} exact />
+                    <Route path='/' component={Reviews} exact />
+                    <Route path='/' component={About} exact />
+                    <Route path='/AboutUs' component={About} />
+                    <Route path='/ProjectGallery' component={Gallery} exact />
+                    <Route path='/ConstructionServices' component={Services} />
+                <Footer />
+            </div>
+        </BrowserRouter>
     );
   }
 }
